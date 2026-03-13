4c75ee4f80712e3b25f5806397e3ddc4ebcaaf30
dimakit <dimakit@gmail.com>
2026-03-13 14:05:58 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './CardPicker.jsx'
+import App from './CardOptimizer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode><App /></StrictMode>
